cc330e44a35365850b51b76c209406b569e74fab
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-08-27 15:33:58 +0300

    Intergrate images in media.

--- portflio/urls.py
+++ portflio/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wangwe/', include('wangwe.urls')),
+    path('', include('wangwe.urls')),
 ]
--- wangwe/urls.py
+++ wangwe/urls.py
@@ -3,5 +3,10 @@ from .views import index
 
 app_name = 'wangwe'
 urlpatterns = [
-    path('', index, name = 'home'),
+    path('', index, name='home'),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
